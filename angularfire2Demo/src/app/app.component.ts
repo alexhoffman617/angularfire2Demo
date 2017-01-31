c942ad4daa9f13c3f1c7e5ca9023f5c27aefd992
Alex Hoffman <alexhoffman617@gmail.com>
2017-01-30 21:31:59 -0500

    Added google login auth

--- angularfire2Demo/src/app/app.component.ts
+++ angularfire2Demo/src/app/app.component.ts
@@ -9,15 +9,24 @@ import { AngularFire, FirebaseListObservable } from 'angularfire2';
 })
 export class AppComponent {
     messages: FirebaseListObservable<any[]>;
+    login() {
+      this.af.auth.login();
+    }
+
+    logout() {
+      this.af.auth.logout();
+    }
+    af;
     submitMessage(time, name, text){
+      var username = this.af.auth._events[0].value ? this.af.auth._events[0].value.google.displayName: 'anonymous'
          this.messages.push({
            time: time,
-           name: name,
+           name: username,
            text: text
          });
     };
     constructor(af: AngularFire) {
+        this.af = af;
          this.messages = af.database.list('/messages');
-
   }
 }
--- angularfire2Demo/src/app/app.module.ts
+++ angularfire2Demo/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { AngularFireModule } from 'angularfire2';
+import { AngularFireModule, AuthProviders, AuthMethods, AngularFire} from 'angularfire2';
+
 
 export const firebaseConfig = {
     apiKey: "AIzaSyAxb2bqyFPng5Tee0MZrHZ5PbACPN2_zCg",
@@ -11,13 +12,20 @@ export const firebaseConfig = {
     messagingSenderId: "1088505020716"
 };
 
+const myFirebaseAuthConfig = {
+  provider: AuthProviders.Google,
+  method: AuthMethods.Redirect,
+  remember: 'default', scope: ['email','id','name','picture']
+};
+
 @NgModule({
   imports: [
     BrowserModule,
-    AngularFireModule.initializeApp(firebaseConfig)
+    AngularFireModule.initializeApp(firebaseConfig, myFirebaseAuthConfig)
   ],
   declarations: [ AppComponent ],
   bootstrap: [ AppComponent ]
 })
 
-export class AppModule { }
+export class AppModule { 
+}
